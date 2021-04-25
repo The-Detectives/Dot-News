40ebb69643a65cc2c5296a39417c3d18f88c4410
Wesam Al-Masri <mr0virus@gmail.com>
2021-04-25 16:21:09 +0300

    structure of the files

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ app.use(
 );
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true }));
 
 //Test Page (Home)
